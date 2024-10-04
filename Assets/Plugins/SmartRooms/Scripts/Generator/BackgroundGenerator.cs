64dc556fad6a7a588398ed50fad54121f34d1450
Daniel Necesal <necesal.daniel@gmail.com>
2024-10-04 16:12:21 +0200

    Fixed background level generation and upgraded unity version

--- Assets/Plugins/SmartRooms/Scripts/Generator/BackgroundGenerator.cs
+++ Assets/Plugins/SmartRooms/Scripts/Generator/BackgroundGenerator.cs
@@ -21,6 +21,9 @@ namespace SmartRooms.Generator
             BackgroundSpriteRenderer.sprite = _levelStyle.BackgroundSprite;
             BackgroundSpriteRenderer.size = new Vector2(tilemapSize.x + _padding.x * 2, tilemapSize.y + _padding.y * 2);
             BackgroundSpriteRenderer.color = _levelStyle.BackgroundColor;
+
+            Vector2 size = BackgroundSpriteRenderer.size;
+            BackgroundSpriteRenderer.transform.position = new Vector3(size.x / 2f - _padding.x, size.y / 2f - _padding.y, 0);
         }
     }
 }
